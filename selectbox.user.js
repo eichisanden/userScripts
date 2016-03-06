5e2dae31ef8b2360545796a206cf89dd775030a2
eichisanden <eichisanden@gmail.com>
2016-03-06 13:03:05 +0900

    add keyword match

--- selectbox.user.js
+++ selectbox.user.js
@@ -8,13 +8,32 @@
 
 'use strict';
 
-var selects = document.getElementsByTagName('select');
-for (let i = 0; i < selects.length; i++) {
-  let select = selects[i];
-  for (let j = select.options.length - 1; j >= 0; j--) {
-    let option = select.options[j];
-    if (!option.selected) {
-	    select.removeChild(option);
-	  }
+(() => {
+  // get target select box as you like.
+  let selects = document.getElementsByTagName('select');
+  // var selects = document.getElementsByClassName('');
+  // var selects = document.getElementById('');
+  // var selects = document.getElementsByName('');
+
+  // filter
+  let include = /.*/;
+  let exclude = /input regexp as you want to exclude/;
+
+  for (let i = 0; i < selects.length; i++) {
+    let select = selects[i];
+    for (let j = select.options.length - 1; j >= 0; j--) {
+      let option = select.options[j];
+
+      // Remove option when exclude matches.
+      if (exclude.test(option.innerHTML)) {
+        select.removeChild(option);
+        continue;
+      }
+
+      // Remove option when include not matches.
+      if (!include.test(option.innerHTML)) {
+        select.removeChild(option);
+      }
+    }
   }
-}
+})();
